b53055e263b072a0333fbb2f6646bd68def493de
nozomi <x8ojo8x@gmail.com>
2023-08-15 23:29:55 +0900

    erbのページを追加してみた

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/' do
   'hello'
+end
+
+## これはお団子ボード用のページ
+get '/board' do
+  'this is your board'
+end
+
+## お団子(Todo)のリストを追加するページ
+
+## 追加したリストを確認するページ
+
+## リストを編集するページ
+
+## 
+
+get '/about' do
+  erb :about
 end
